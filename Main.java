8630d4e67534d97a550bf14570e6604c9dfaac97
Rnxp <90277569+SrXitme@users.noreply.github.com>
2024-04-08 21:21:19 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,6 +4,8 @@ import java.net.http.HttpResponse;
 import java.net.URI;
 import java.io.IOException;
 import java.util.Scanner;
+import Laura;
+
 
 import java.io.*;
 
@@ -28,7 +30,7 @@ public class Main {
 		    String body = response.body();
 		    
 		    System.out.println(body);
-		    try_codes(
+		    Laura_patten.try_codes(
 			    {
 			    2261, 8427,
                             0446, 8139,
@@ -39,7 +41,17 @@ public class Main {
                             6269, 6753,
                             7713, 5704,
                             5056, 3673,
-                            0149, 2872
+                            0149, 2872,
+			    "6797 0711
+                             2886 8428
+                             5928 7748
+                             3754 8612
+                             4297 7448
+                             6269 3136
+                             5330 4100
+                             9587 6182
+                             9987 9134
+                             8524 4652"
 			    }
 		    );
 		    saveFile(body);
